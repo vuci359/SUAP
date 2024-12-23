1bd96beacc1cbacfe5e075aeee93c91ddbe30a0a
Karlo Vučilovski <karlo.vucilovski@fer.hr>
2024-12-23 14:36:49 +0100

    dodan opis koji služi za stvaranje dokumentacije

--- suap_end_device/main/lvgl_demo_ui.c
+++ suap_end_device/main/lvgl_demo_ui.c
@@ -12,6 +12,7 @@
 void example_lvgl_demo_ui(lv_disp_t *disp)
 {
     lv_obj_t *scr = lv_disp_get_scr_act(disp);
+    lv_group_t * g = lv_group_create();
     /*Create a menu object*/
     lv_obj_t * menu = lv_menu_create(scr);
     lv_obj_set_size(menu, lv_disp_get_hor_res(NULL), lv_disp_get_ver_res(NULL));
@@ -42,6 +43,7 @@ void example_lvgl_demo_ui(lv_disp_t *disp)
     label = lv_label_create(cont);
     lv_label_set_text(label, "Item 3 (Click me!)");
     lv_menu_set_load_page_event(menu, cont, sub_page);
+    lv_group_add_obj(g, main_page);
 
     lv_menu_set_page(menu, main_page);
 }
