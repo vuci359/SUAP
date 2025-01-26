7fa65edf591a09fa5db65c69f10c55eb164af2a4
Karlo Vučilovski <karlo.vucilovski@fer.hr>
2025-01-26 23:28:31 +0100

    sitni ispravci  i dopune

--- suap_api_server/suap_api_server/obj/Debug/net9.0/suap_api_server.AssemblyInfo.cs
+++ suap_api_server/suap_api_server/obj/Debug/net9.0/suap_api_server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("suap_api_server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d619aef195a0207c5a77e42c2f17f13a520e797d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ebc5fae65054936a4f8ea9ee5294f4631326ff1")]
 [assembly: System.Reflection.AssemblyProductAttribute("suap_api_server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("suap_api_server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
